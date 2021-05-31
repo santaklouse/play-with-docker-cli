92aabf2d82cc24336b07e2cc14aada771797c6ec
Alex Nevpryaga <alexnevpryaga@gmail.com>
2021-05-31 15:55:29 -0400

    add checking of PWD instance health; fix npm

--- pwd-cli.sh
+++ pwd-cli.sh
@@ -1,14 +1,35 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 #export PWD_URL='https://labs.play-with-docker.com/p/c2aerennjsv000eej7vg'
 #export PWD_URL='https://labs.play-with-docker.com/p/c2n1tt7qf8u000atomhg#c2n1tt7q_c2n1u17qf8u000atomk0'
 #export PWD_URL=`../pwd/browser.js`
 
+DEBUG=
+DEBUG=$([[ $# > 0 ]] || echo 1)
+
+[ ! -z $DEBUG ]  && echo  ''
+[ ! -z $DEBUG ]  && echo -n "Checking PWD instance URL..."
+PWD_URL=`cat .pwd_url`
 if [ -z "${PWD_URL}" ]; then
+    [ ! -z $DEBUG ]  && echo -n 'Missing, recreating instance...'
     . ./pwd-create-instance.sh
     export PWD_URL
+    [ ! -z $DEBUG ]  && echo 'Done.'
+else
+    [ ! -z $DEBUG ]  && echo 'Done.'
 fi
 
+# check PWD validity
+[ ! -z $DEBUG ] && echo -n 'Checking validity...'
+RESPONSE_CODE=`curl $PWD_URL -w '%{response_code}'  -o /dev/null -s`
+[ ! -z $DEBUG ] && echo "${RESPONSE_CODE} code"
+if [[ $RESPONSE_CODE != "200" ]]; then
+    [ ! -z $DEBUG ] && echo -n 'Recreating instance...'
+    . ./pwd-create-instance.sh
+    export PWD_URL
+    [ ! -z $DEBUG ] && echo 'Done.'
+fi
+export PWD_URL
 torsocks ./pwd-cli $@
 
 #ssh trought tor -> pwd node
--- pwd-create-instance.sh
+++ pwd-create-instance.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export PWD_URL=`./create-instance/browser.js`
-
+echo $PWD_URL > .pwd_url
 echo "Created: ${PWD_URL}"
